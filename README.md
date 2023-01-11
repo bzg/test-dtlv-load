88e90f024b4c1a625473b650083622876fbb46f1
Bastien <bzg@gnu.org>
2023-01-12 00:00:14 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # test-dtlv-load
-Test repo to try loading and indexing data for full-text search
+
+See https://github.com/juji-io/datalevin/issues/151
+
+Clone and run this script:
+
+    ~$ ./test.clj
+	
+Last timed results:
+
+```
+1000: 1,64s user 0,17s system 96% cpu 1,870 total
+2000: 5,50s user 0,14s system 97% cpu 5,789 total
+3000: 12,19s user 0,17s system 98% cpu 12,569 total
+4000: 20,96s user 0,23s system 98% cpu 21,472 total
+5000: 32,70s user 0,22s system 99% cpu 33,247 total
+```
---
+++ test.clj
@@ -0,0 +1,30 @@
+#!/usr/bin/env bb
+
+;; Require pods and datalevin pod
+(require '[babashka.pods :as pods])
+(require '[babashka.cli :as cli])
+(pods/load-pod 'huahaiy/datalevin "0.7.12")
+(require '[pod.huahaiy.datalevin :as d])
+
+;; Create the database
+(def schema {:id          {:db/valueType :db.type/string
+                           :db/unique    :db.unique/identity}
+             :description {:db/valueType :db.type/string
+                           :db/fulltext  true}})
+(def conn (d/get-conn "/tmp/test-dtlv" schema))
+(def db (d/db conn))
+
+;; Get the data
+(def dtg-raw-data (csv/read-csv (slurp "data.csv")))
+
+;; Return data rows as maps
+(defn- rows->maps [csv]
+  (let [headers (map keyword (first csv))
+        rows    (rest csv)]
+    (map #(zipmap headers %) rows)))
+
+;; Add data to the db
+(d/transact! conn (rows->maps dtg-raw-data))
+
+(comment
+  (count (d/q '[:find ?a :where [?a :id _]] db)))
