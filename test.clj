5add35d064e9ca4efe07e6dace3b92855d276c69
Bastien <bzg@gnu.org>
2023-01-19 13:50:47 +0100

    test.clj: Bump datalevin version to 0.8.0

--- test.clj
+++ test.clj
@@ -3,7 +3,7 @@
 ;; Require pods and datalevin pod
 (require '[babashka.pods :as pods])
 (require '[babashka.cli :as cli])
-(pods/load-pod 'huahaiy/datalevin "0.7.12")
+(pods/load-pod 'huahaiy/datalevin "0.8.0")
 (require '[pod.huahaiy.datalevin :as d])
 
 ;; Create the database
