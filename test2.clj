7395d0a14ccb71bae7248a59a8ebea422b603e1b
Bastien <bzg@gnu.org>
2023-01-20 10:48:43 +0100

    test2.clj: Bisect error with 0.8.2

--- test2.clj
+++ test2.clj
@@ -19,6 +19,14 @@
 (def all  (json/parse-string (slurp "all.json") true))
 
 ;; Feed the db
+;; (d/transact! conn (take 7555 all)) ;; Works okay
+;; (d/transact! conn (take 7556 all)) ;; Fails systematically
+
+;; The culprit seems to be this entry, for which there is nothing
+;; wrong:
+
+;; {"id" : 648, "name" : "txm-manual", "organization_name" : "txm", "platform" : "GitLab", "repository_url" : "https://gitlab.huma-num.fr/txm/txm-manual", "description" : "TXM User's Manual writing and online hosting", "default_branch" : "master", "is_fork" : "", "is_archived" : false, "creation_date" : "2020-04-14T14:49:37Z", "last_update" : "2022-05-03T15:27:53Z", "last_modification" : "2022-05-03T15:27:53Z", "homepage" : "", "stars_count" : 0, "forks_count" : 0, "license" : "", "open_issues_count" : 0, "language" : "", "topics" : "", "software_heritage_exists" : false, "software_heritage_url" : ""}
+
 (d/transact! conn all)
 
 (comment
